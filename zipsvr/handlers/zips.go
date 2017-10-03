84c6ca25f2aa47ee18815768b8c6c2443da490f5
Pavan Kumar <pavank1@uw.edu>
2017-10-03 15:45:22 -0700

    ..

--- zipsvr/handlers/zips.go
+++ zipsvr/handlers/zips.go
@@ -2,9 +2,10 @@ package handlers
 
 import (
 	"encoding/json"
-	"info344-in-class/zipsvr/models"
 	"net/http"
 	"strings"
+
+	"github.com/pavankUW/info344-in-class/zipsvr/models"
 )
 
 type CityHandler struct {
