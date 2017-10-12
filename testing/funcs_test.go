00cc232b77d72b184c70f7b279271b5405fa2f4d
Pavan Kumar <pavank1@uw.edu>
2017-10-12 14:54:29 -0700

    some test

--- testing/funcs_test.go
+++ testing/funcs_test.go
@@ -5,17 +5,97 @@ import (
 )
 
 func TestReverse(t *testing.T) {
+	cases := []struct {
+		name           string
+		input          string
+		expectedOutput string
+	}{
+		{
+			name:           "empty string",
+			input:          "",
+			expectedOutput: "",
+		},
+		{
+			name:           "single character",
+			input:          "a",
+			expectedOutput: "a",
+		},
+		{
+			name:           "two characters",
+			input:          "ab",
+			expectedOutput: "ba",
+		},
+		{
+			name:           "palindrome",
+			input:          "stressed",
+			expectedOutput: "desserts",
+		},
+		{
+			name:           "high unicode",
+			input:          "Hello, 世界",
+			expectedOutput: "界世 ,olleH",
+		},
+	}
 
+	for _, c := range cases {
+		if output := Reverse(c.input); output != c.expectedOutput {
+			t.Errorf("%s: got %s but expected %s", c.name, output, c.expectedOutput)
+		}
+	}
 }
 
 func TestGetGreeting(t *testing.T) {
-
+	cases := []struct {
+		name           string
+		input          string
+		expectedOutput string
+	}{
+		{
+			name:           "regular greeting",
+			input:          "John",
+			expectedOutput: "Hello, John!",
+		},
+		{
+			name:           "empty",
+			input:          "",
+			expectedOutput: "Hello, World!",
+		},
+	}
+	for _, c := range cases {
+		if output := GetGreeting(c.input); output != c.expectedOutput {
+			t.Errorf("%s got %s but expected %s", c.name, output, c.expectedOutput)
+		}
+	}
 }
 
 func TestParseSize(t *testing.T) {
-
+	cases := []struct {
+		name           string
+		input          string
+		expectedOutput *Size
+	}{
+		{
+			name:           "empty string",
+			input:          "",
+			expectedOutput: &Size{},
+		},
+	}
+	for _, c := range cases {
+		if output := ParseSize(c.input); output.Height != c.expectedOutput.Height || output.Width != c.expectedOutput.Width {
+			t.Errorf("%s: got %v but expeceted %v", c.name, output, c.expectedOutput)
+		}
+	}
 }
 
 func TestLateDaysConsume(t *testing.T) {
-
+	ld := NewLateDays()
+	for i := 3; i > -10; i-- {
+		expectedLateDays := i
+		if expectedLateDays < 0 {
+			expectedLateDays = 0
+		}
+		if output := ld.Consume("test"); output != expectedLateDays {
+			t.Errorf("iteration %d got %d but expected %d", i, output, expectedLateDays)
+		}
+	}
 }
