a464e29fe7262f07135eaa345e6ea35ea44e49ea
Pavan Kumar <pavank1@uw.edu>
2017-10-24 02:01:21 -0700

    in class

--- hmac/main_test.go
+++ hmac/main_test.go
@@ -1,12 +1,18 @@
 package main
 
 import "testing"
+import "io"
 
 func TestSign(t *testing.T) {
 	//TODO: write unit test cases for sign()
 	//use strings.NewReader() to get an io.Reader
 	//interface over a simple string
 	//https://golang.org/pkg/strings/#NewReader
+	cases := []struct {
+		key string
+		stream io.Reader
+		
+	}
 }
 
 func TestVerify(t *testing.T) {
